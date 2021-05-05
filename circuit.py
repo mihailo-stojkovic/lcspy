c8eb7a9d9323dc0bd57d9fb4714907cb4da9559b
mihailo-stojkovic <mihailostojkovicpkr@gmail.com>
2021-05-05 16:11:10 +0200

    Circuit class definition.

---
+++ circuit.py
@@ -0,0 +1,18 @@
+
+
+#Main circuit class used for adding new elements to the circuit, analyzing the circuit
+#and getting responses from the circuit
+
+class Circuit:
+	
+	def __init__(self):
+		pass
+
+	def add(self, instruction):
+		pass
+
+	def getI(self, node1, node2):
+		pass
+
+	def getV(self, node1, node2):
+		pass
